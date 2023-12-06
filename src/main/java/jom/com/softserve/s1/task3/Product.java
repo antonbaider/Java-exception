2f573e0c85864a4b38673ac3a75e6d3ea525659e
antonbaider <email.b.anton@gmail.com>
2023-12-06 15:08:29 +0100

    task done

--- src/main/java/jom/com/softserve/s1/task3/Product.java
+++ src/main/java/jom/com/softserve/s1/task3/Product.java
@@ -1,5 +1,37 @@
 package jom.com.softserve.s1.task3;
 
 public class Product {
+    static int counts = 0;
+    private String name;
+    private double price;
 
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+        counts++;
+    }
+
+    public Product() {
+        counts++;
+    }
+
+    public static int count() {
+        return counts;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
