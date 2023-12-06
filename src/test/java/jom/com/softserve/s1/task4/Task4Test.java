de3ec72718e3cff086c3b62a31048ed348eebd51
antonbaider <email.b.anton@gmail.com>
2023-12-06 15:35:35 +0100

    task done

--- src/test/java/jom/com/softserve/s1/task4/Task4Test.java
+++ src/test/java/jom/com/softserve/s1/task4/Task4Test.java
@@ -20,99 +20,99 @@ class Task4Test {
 
 	final private static String OPERATION = "jom.com.softserve.s1.task4.Operation";
 
-//	@DisplayName("Check that Product class is present")
-//	@Test
-//	void isTypeClass() {
-//		try {
-//			assertNotNull(Class.forName(OPERATION));
-//			assertEquals("Operation", Class.forName(OPERATION).getSimpleName());
-//		} catch (ClassNotFoundException e) {
-//			fail("There is no Operation class");
-//		}
-//	}
-//
-//	@DisplayName("Check that a class has a properly methods")
-//	@ParameterizedTest
-//	@MethodSource("provideTypeDeclaredMethod")
-//	void hasTypeDeclaredMethod(String methodName, Class[] paramTypes) {
-//		try {
-//			Class<?> clazz = Class.forName(OPERATION);
-//			Method[] methods = clazz.getDeclaredMethods();
-//			boolean isMethod = false;
-//			Class<?>[] types = paramTypes;
-//			for (Method method : methods) {
-//				if (methodName.equals(method.getName())) {
-//					Class<?>[] parameterTypes = method.getParameterTypes();
-//					if (Arrays.equals(types, parameterTypes)) {
-//						isMethod = true;
-//					}
-//				}
-//			}
-//			assertTrue(isMethod);
-//		} catch (ClassNotFoundException e) {
-//			fail("There is no such class");
-//		}
-//
-//	}
-//
-//	private static Stream<Arguments> provideTypeDeclaredMethod() {
-//		return Stream.of(Arguments.of("squareRectangle", new Class[] { int.class, int.class }),
-//				Arguments.of("trySquareRectangle", new Class[] { int.class, int.class }));
-//	}
-//
-//	@DisplayName("Check that method returns value of appropriate type")
-//	@ParameterizedTest
-//	@MethodSource("provideReturnTypeDeclaredMethod")
-//	void hasMethodReturnType(String methodName, Class type) {
-//		try {
-//			Class<?> clazz = Class.forName(OPERATION);
-//			Method[] methods = clazz.getDeclaredMethods();
-//			boolean isMethod = false;
-//			for (Method method : methods) {
-//				if (methodName.equals(method.getName())) {
-//					assertEquals(method.getReturnType(), type);
-//					isMethod = true;
-//				}
-//			}
-//			assertTrue(isMethod);
-//		} catch (ClassNotFoundException e) {
-//			fail("There is no such class");
-//		}
-//	}
-//
-//	private static Stream<Arguments> provideReturnTypeDeclaredMethod() {
-//		return Stream.of(Arguments.of("squareRectangle", int.class), Arguments.of("trySquareRectangle", int.class));
-//	}
-//
-//	@DisplayName("Check that 'squareRectangle' method throws an exception")
-//	@ParameterizedTest
-//	@MethodSource("providesquareRectangleMethod")
-//	void squareRectangleTest(int a, int b) {
-//		assertThrows(IllegalArgumentException.class, () -> {
-//			Operation.squareRectangle(a, b);
-//		}, "both arguments should be more than zero");
-//	}
-//
-//	private static Stream<Arguments> providesquareRectangleMethod() {
-//		return Stream.of(Arguments.of(-3, 5), Arguments.of(-3, -5), Arguments.of(3, -5));
-//	}
-//
-//	@DisplayName("Check the result of 'squareRectangle' method")
-//	@Test
-//	void squareRectanglePositiveTest() {
-//		assertTrue(Operation.squareRectangle(3, 5) == 15);
-//	}
-//
-//	@DisplayName("Check the result of 'trySquareRectangle' method")
-//	@ParameterizedTest
-//	@MethodSource("providesquareRectangleMethod")
-//	void trySquareRectangleTest(int a, int b) {
-//		assertTrue(Operation.trySquareRectangle(a, b) == -1);
-//	}
-//
-//	@DisplayName("Check the result of 'trySquareRectangle' method")
-//	@Test
-//	void trySsquareRectanglePositiveTest() {
-//		assertTrue(Operation.trySquareRectangle(3, 5) == 15);
-//	}
+	@DisplayName("Check that Product class is present")
+	@Test
+	void isTypeClass() {
+		try {
+			assertNotNull(Class.forName(OPERATION));
+			assertEquals("Operation", Class.forName(OPERATION).getSimpleName());
+		} catch (ClassNotFoundException e) {
+			fail("There is no Operation class");
+		}
+	}
+
+	@DisplayName("Check that a class has a properly methods")
+	@ParameterizedTest
+	@MethodSource("provideTypeDeclaredMethod")
+	void hasTypeDeclaredMethod(String methodName, Class[] paramTypes) {
+		try {
+			Class<?> clazz = Class.forName(OPERATION);
+			Method[] methods = clazz.getDeclaredMethods();
+			boolean isMethod = false;
+			Class<?>[] types = paramTypes;
+			for (Method method : methods) {
+				if (methodName.equals(method.getName())) {
+					Class<?>[] parameterTypes = method.getParameterTypes();
+					if (Arrays.equals(types, parameterTypes)) {
+						isMethod = true;
+					}
+				}
+			}
+			assertTrue(isMethod);
+		} catch (ClassNotFoundException e) {
+			fail("There is no such class");
+		}
+
+	}
+
+	private static Stream<Arguments> provideTypeDeclaredMethod() {
+		return Stream.of(Arguments.of("squareRectangle", new Class[] { int.class, int.class }),
+				Arguments.of("trySquareRectangle", new Class[] { int.class, int.class }));
+	}
+
+	@DisplayName("Check that method returns value of appropriate type")
+	@ParameterizedTest
+	@MethodSource("provideReturnTypeDeclaredMethod")
+	void hasMethodReturnType(String methodName, Class type) {
+		try {
+			Class<?> clazz = Class.forName(OPERATION);
+			Method[] methods = clazz.getDeclaredMethods();
+			boolean isMethod = false;
+			for (Method method : methods) {
+				if (methodName.equals(method.getName())) {
+					assertEquals(method.getReturnType(), type);
+					isMethod = true;
+				}
+			}
+			assertTrue(isMethod);
+		} catch (ClassNotFoundException e) {
+			fail("There is no such class");
+		}
+	}
+
+	private static Stream<Arguments> provideReturnTypeDeclaredMethod() {
+		return Stream.of(Arguments.of("squareRectangle", int.class), Arguments.of("trySquareRectangle", int.class));
+	}
+
+	@DisplayName("Check that 'squareRectangle' method throws an exception")
+	@ParameterizedTest
+	@MethodSource("providesquareRectangleMethod")
+	void squareRectangleTest(int a, int b) {
+		assertThrows(IllegalArgumentException.class, () -> {
+			Operation.squareRectangle(a, b);
+		}, "both arguments should be more than zero");
+	}
+
+	private static Stream<Arguments> providesquareRectangleMethod() {
+		return Stream.of(Arguments.of(-3, 5), Arguments.of(-3, -5), Arguments.of(3, -5));
+	}
+
+	@DisplayName("Check the result of 'squareRectangle' method")
+	@Test
+	void squareRectanglePositiveTest() {
+		assertTrue(Operation.squareRectangle(3, 5) == 15);
+	}
+
+	@DisplayName("Check the result of 'trySquareRectangle' method")
+	@ParameterizedTest
+	@MethodSource("providesquareRectangleMethod")
+	void trySquareRectangleTest(int a, int b) {
+		assertTrue(Operation.trySquareRectangle(a, b) == -1);
+	}
+
+	@DisplayName("Check the result of 'trySquareRectangle' method")
+	@Test
+	void trySsquareRectanglePositiveTest() {
+		assertTrue(Operation.trySquareRectangle(3, 5) == 15);
+	}
 }
