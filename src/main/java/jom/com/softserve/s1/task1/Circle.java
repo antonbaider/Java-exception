6d13d8bdd99b5d0bb6fee8014133c7318336f603
antonbaider <email.b.anton@gmail.com>
2023-12-06 13:24:35 +0100

    task 1

--- src/main/java/jom/com/softserve/s1/task1/Circle.java
+++ src/main/java/jom/com/softserve/s1/task1/Circle.java
@@ -1,14 +1,25 @@
 package jom.com.softserve.s1.task1;
 
 class Circle {
-	private double radius;
+    private double radius;
 
-	public Circle(double radius) {
-		this.radius = radius;
-	}
+    public Circle(double radius) {
+        this.radius = radius;
+    }
 
-	public void draw() {
-		// Draw Square with default color and radius
-	}
-	//your code
+    public void draw() {
+        // Draw Square with default color and radius
+    }
+
+    public void draw(String color) {
+    }
+
+    public void draw(float scale) {
+
+    }
+
+    public void draw(String color, float scale) {
+
+    }
+
 }
