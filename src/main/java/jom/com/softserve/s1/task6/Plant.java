83ef38e7dcf409110ec728dfd59358122be9e381
antonbaider <email.b.anton@gmail.com>
2023-12-06 20:55:01 +0100

    task done

--- src/main/java/jom/com/softserve/s1/task6/Plant.java
+++ src/main/java/jom/com/softserve/s1/task6/Plant.java
@@ -1,5 +1,22 @@
 package jom.com.softserve.s1.task6;
 
 public class Plant {
+    public static Plant tryCreatePlant(String type, String color, String name) throws ColorException, TypeException {
+        try {
+            return new Plant(type, color, name);
+        } catch (ColorException ex) {
+            try {
+                return new Plant(type, "Red", name);
+            } catch (ColorException | TypeException exs) {
+                return new Plant("Ordinary", "Red", name);
+            }
 
+        } catch (TypeException ex) {
+            try {
+                return new Plant("Ordinary", color, name);
+            } catch (ColorException | TypeException exs) {
+                return new Plant("Ordinary", "Red", name);
+            }
+        }
+    }
 }
